a9d2b26bbdbcc3bd6eff77563bb41ede24cd934a
Jose Rojas <jose.rojas@bridgevine.com>
2015-05-11 10:23:11 -0400

    Keep it in synch

---
+++ InitDocsGit.bat
@@ -0,0 +1,10 @@
+echo # BV_Documents >> README.md
+
+git init
+git add README.md
+
+git commit -m "first commit"
+
+git remote add origin https://github.com/jfrojasdelrio/BV_Documents.git
+
+git push -u origin master
